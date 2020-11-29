9a3ea7b3b417b03384a193b572cf1454efe858d4
coloryourlife <coloryourlife319@gmail.com>
2020-11-30 03:01:43 +0900

    modified; ;

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
 		// console.log(Cookies)
 		// Cookies.remove("session");
 		// history.push('/')
-		fetch('http://127.0.0.1:5000/auth/sessionLogout', {
+		fetch('http://13.209.98.249:5000//auth/sessionLogout', {
 			method: 'POST',
 			credentials: "include",
 		}).then(res => {
